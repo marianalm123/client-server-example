07ea7153b8c50e3b7f1b5b6b55227147d2a83e02
marianalm123 <marianaleitemelo4@gmail.com>
2021-08-28 10:05:03 +0100

    first commit of my project

---
+++ packages/server/src/main.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const server = express();
+
+server.get('/status', (_, response) => {
+    response.send({
+        status: 'Okay',
+    });
+});
+
+server.post('/authenticate', express.json(), (request, response) => {
+    //request.body
+    response.send();
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+});
